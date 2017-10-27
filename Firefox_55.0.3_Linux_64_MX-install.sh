ce33a1c74c283aeb86facb236eb3149cd7acb3ce
Miguel A. De Paz <31639324+xaxlm@users.noreply.github.com>
2017-10-26 22:56:13 -0500

    Update Firefox_55.0.3_Linux_64_MX-install.sh

--- Firefox_55.0.3_Linux_64_MX-install.sh
+++ Firefox_55.0.3_Linux_64_MX-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Última actualización: 21/10/2017
+# Última actualización: 26/10/2017
 cd /
 # Se descarga Firefox
-wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/56.0.1/linux-x86_64/es-MX/firefox-56.0.1.tar.bz2
+wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/56.0.2/linux-x86_64/es-MX/firefox-56.0.2.tar.bz2
 cd /usr/local/
 echo 'Se va a instalar en /usr/local/...'
 read -p "Ingresa el nombre del directorio principal de instalacion, si no existe se creará: " DIRINST
@@ -20,14 +20,14 @@ if [ -d $DIRINST ];
 		cd $DIRINST
 fi
 sleep 2
-mv /firefox-56.0.1.tar.bz2 /usr/local/$DIRINST
-tar jvxf firefox-56.0.1.tar.bz2
+mv /firefox-56.0.2.tar.bz2 /usr/local/$DIRINST
+tar jvxf firefox-56.0.2.tar.bz2
 #
 #App a Menú
 cd /usr/share/applications/
 touch firefox.desktop
 echo '[Desktop Entry]' > firefox.desktop
-echo 'Version=56.0.1' >> firefox.desktop
+echo 'Version=56.0.2' >> firefox.desktop
 echo 'Name=Mozilla Firefox' >> firefox.desktop
 echo 'Comment=Suite Navegador Web' >> firefox.desktop
 echo 'Exec=/usr/local/'$DIRINST'/firefox/firefox' >> firefox.desktop
@@ -69,7 +69,7 @@ chmod 744 remove.sh
 touch README.txt
 echo 'Mozilla Firefox' > README.txt
 echo 'Idioma=es_MX' >> README.txt
-echo 'Version=56.0.1' >> README.txt
+echo 'Version=56.0.2' >> README.txt
 echo 'Arquitectura=x86_64' >> README.txt
 echo 'Esta es una instalación no invasiva.' >> README.txt
 echo 'Para desinstalar ejecute el fichero remove.sh como root.' >> README.txt
@@ -95,7 +95,7 @@ echo -ne '[=================================>      ](83.30%)\r';sleep 0.2
 echo -ne '[====================================>   ](91.64%)\r';sleep 0.2
 echo -ne '[=======================================>](100.00%)\r';sleep 0.2
 echo -ne '\n'
-rm -rf /usr/local/$DIRINST/firefox-56.0.1.tar.bz2
+rm -rf /usr/local/$DIRINST/firefox-56.0.2.tar.bz2
 if [ -d /home/*/.cache/mozilla/firefox/ ];
 	then
 		rm -rf /home/*/.cache/mozilla/firefox/
